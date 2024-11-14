c3bc0f706842e35f96cfe12438ae906e392deb29
Himanshu Pathak <pathakvishal132@gmail.com>
2024-11-14 21:43:33 +0530

    changed api url

--- src/app/services/questions.service.ts
+++ src/app/services/questions.service.ts
@@ -7,7 +7,8 @@ import { Observable } from 'rxjs';
 })
 export class QuestionsService {
 
-  private apiUrl = 'http://127.0.0.1:8000/api';
+  // private apiUrl = 'http://127.0.0.1:8000/api';
+  private apiUrl = 'https://interview-prep-hub-backend-three.vercel.app/api';
   private token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI2NTcyNzczLCJpYXQiOjE3MjY1NjA3NzMsImp0aSI6ImM1NzJlNmYzZGY1NzQyMjQ4MzcyMzRmYjIyNTM2MjNjIiwidXNlcl9pZCI6Nn0.utWE1D4yZnK9sHg1IiezZhR2xukpKaxSBeCbJPLKrC8';
 
   constructor(private http: HttpClient) { }
@@ -28,7 +29,4 @@ export class QuestionsService {
     console.log(payload);
     return this.http.post<any>(`${this.apiUrl}/get_feedback/`, payload);
   }
-
-
-
 }
