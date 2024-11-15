c3fb486b25b0f758e391d02c93255e37efa550e1
Himanshu Pathak <pathakvishal132@gmail.com>
2024-11-15 21:06:53 +0530

    Modified all domain feilds

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -6,8 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AdminService {
-
-  private apiUrl = 'http://127.0.0.1:8000/api/';
+  // private apiUrl = 'http://127.0.0.1:8000/api';
+  private apiUrl = 'https://interview-prep-hub-backend-three.vercel.app/api';
 
 
   constructor(private http: HttpClient) { }
