19b7942d74078cf8d5ce36061f021cbab3e4fe10
Himanshu Pathak <pathakvishal132@gmail.com>
2024-11-17 02:28:10 +0530

    change api url

--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -6,15 +6,13 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class CompanyService {
-  private apiUrl = 'http://127.0.0.1:8000/api';
-  // private apiUrl = 'https://interview-prep-hub-backend-three.vercel.app/api';
+  // private apiUrl = 'http://127.0.0.1:8000/api';
+  private apiUrl = 'https://interview-prep-hub-backend-three.vercel.app/api';
   private token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI2NTcyNzczLCJpYXQiOjE3MjY1NjA3NzMsImp0aSI6ImM1NzJlNmYzZGY1NzQyMjQ4MzcyMzRmYjIyNTM2MjNjIiwidXNlcl9pZCI6Nn0.utWE1D4yZnK9sHg1IiezZhR2xukpKaxSBeCbJPLKrC8';
 
   constructor(private http: HttpClient) { }
 
   getCompany(page: number): Observable<any> {
-    // const payload = { question, answer };
-    // console.log(payload);
     return this.http.get<any>(`${this.apiUrl}/get_company/?page=${page}`);
   }
   deleteCompany(id: number): Observable<any> {
