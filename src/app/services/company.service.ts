9ea94b9afdd2065f60d4adbd9587911412ad0147
Himanshu Pathak <pathakvishal132@gmail.com>
2024-11-19 00:17:22 +0530

    fixed company details issue2

--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -6,8 +6,8 @@ import { catchError } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class CompanyService {
-  private apiUrl = 'http://127.0.0.1:8000/api';
-  // private apiUrl = 'https://interview-prep-hub-backend-three.vercel.app/api';
+  // private apiUrl = 'http://127.0.0.1:8000/api';
+  private apiUrl = 'https://interview-prep-hub-backend-three.vercel.app/api';
   private token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI2NTcyNzczLCJpYXQiOjE3MjY1NjA3NzMsImp0aSI6ImM1NzJlNmYzZGY1NzQyMjQ4MzcyMzRmYjIyNTM2MjNjIiwidXNlcl9pZCI6Nn0.utWE1D4yZnK9sHg1IiezZhR2xukpKaxSBeCbJPLKrC8';
 
   constructor(private http: HttpClient) { }
