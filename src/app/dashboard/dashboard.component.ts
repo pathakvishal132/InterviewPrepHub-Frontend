28b41a6203f1f311e3275c8a9099f36434f6b422
Himanshu Pathak <pathakvishal132@gmail.com>
2026-02-10 18:44:46 +0530

    fixed ping get methood call

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { QuestionsService } from '../services/questions.service';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
-  constructor(private router: Router,
-    private questionService: QuestionsService,
 
-  ) {
-    questionService.ping();
-  }
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private questionService: QuestionsService
+  ) { }
 
+  ngOnInit(): void {
+    this.questionService.ping().subscribe({
+      next: (res) => {
+        console.log('Ping success:', res);
+      },
+      error: (err) => {
+        console.error('Ping failed:', err);
+      }
+    });
   }
+
   navigateToInterviewDashboard() {
-    this.router.navigateByUrl("interview-dashboard");
+    this.router.navigateByUrl('interview-dashboard');
   }
 }
